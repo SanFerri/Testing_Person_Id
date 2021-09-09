0a72468d52fa7993d78b959b1cab7f7f93633dbb
Santiago Ferraro <santiago.ferraro@correo.ucu.edu.uy>
2021-09-09 16:02:06 -0300

    Arreglo de la falla

--- PII_Debug_And_Test_Person/test/Library.Test/PersonTests.cs
+++ PII_Debug_And_Test_Person/test/Library.Test/PersonTests.cs
@@ -1,5 +1,4 @@
 using NUnit.Framework;
-
 using UnitTestAndDebug;
 
 namespace Tests
@@ -9,13 +8,30 @@ namespace Tests
         [SetUp]
         public void Setup()
         {
-            // Insertá tu código de inicialización aquí
+            Person person = new Person("Santiago Ferraro", "5.307.722-8");
         }
-
         [Test]
-        public void Test1() // Cambiá el nombre para indicar qué estás probando
+        public void TestRealId() // Probamos con una ID real que es la de arriba si efectivamente sirvio.
         {
-            // Insertá tu código  de pruebaaquí
+            Person person = new Person("Santiago Ferraro", "5.307.722-8");
+            const string expected = "5.307.722-8";
+            Assert.AreEqual(expected, person.ID);
+        }
+        [Test]
+        public void TestIdUtils_Length() // Verificamos que el isvalid funcione para verificar el largo de un Id. 
+        {
+            Person person = new Person("Santiago Ferraro", "5.307.722-8");
+            const string expected = "5.307.722-8";
+            person.ID = "5.07.722-8";
+            Assert.AreEqual(expected, person.ID); // Si sirvio el person.ID deberia mantenerse ya que el que se intento poner deberia ser invalido
+        }
+        [Test]
+        public void TestIdUtils_CheckDigit() // Verificamos que el isvalid funcione para verificar el numero verificador.
+        {
+            Person person = new Person("Santiago Ferraro", "5.307.722-8");
+            const string expected = "5.307.722-8";
+            person.ID = "5.307.722-4";
+            Assert.AreEqual(expected, person.ID); // Si sirvio el person.ID deberia mantenerse ya que el que se intento poner deberia ser invalido
         }
     }
 }
